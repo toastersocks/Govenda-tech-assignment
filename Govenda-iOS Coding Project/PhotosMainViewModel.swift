063c0f18e9f343487c3adebe815e3646c8edd806
James Pamplona <pamplonajames@gmail.com>
2023-08-15 15:19:03 -0700

    Add basic photos flow layout collection view

---
+++ Govenda-iOS Coding Project/PhotosMainViewModel.swift
@@ -0,0 +1,88 @@
+//
+//  PhotosMainViewModel.swift
+//  Govenda-iOS Coding Project
+//
+//  Created by James Pamplona on 8/14/23.
+//
+
+import UIKit
+
+
+class PhotosMainViewModel: ObservableObject {
+    private let photosProvider: PhotoProvider
+
+    @Published private(set)
+    var photos: [PhotoViewModel] = []
+
+    /*
+    lazy var photosStream: AsyncStream<[PhotoViewModel]> = AsyncStream(unfolding: { [weak self] in
+        guard let self else { return nil }
+
+        do {
+            return try await photosProvider.curatedPhotos(page: 1, photosPerPage: 4)
+                .map(PhotoViewModel.init)
+        } catch {
+            print(error)
+
+            return nil
+        }
+
+    })
+    */
+
+    init() {
+        guard let photosProvider = PhotoProvider() else { preconditionFailure("Make sure you've set up an api key in APIKey.xcconfig or pass an api key to the PhotoProvider initializer.") }
+
+        self.photosProvider = photosProvider
+    }
+
+    func refreshPhotos(page: Int, photosPerPage: Int) {
+        Task.detached { @MainActor in
+            self.photos = try await self.photosProvider.curatedPhotos(page: page, photosPerPage: photosPerPage)
+                .map(PhotoViewModel.init)
+        }
+    }
+
+    subscript(_ photoIndex: Int) -> PhotoViewModel {
+        photos[photoIndex]
+    }
+}
+
+@dynamicMemberLookup
+class PhotoViewModel {
+    private let photo: Photo
+    var thumbnail: UIImage? {
+        get async throws {
+            let imageData = try Data(contentsOf: self.photo.imageURLs.tiny)
+
+            return UIImage(data: imageData)
+        }
+    }
+
+    var averageColor: UIColor {
+        let hex = String(photo.averageColor.trimmingPrefix(/#/))
+
+        guard hex.count == 6 else { return .clear }
+
+        let scanner = Scanner(string: hex)
+        var hexNumber: UInt64 = 0
+
+        guard scanner.scanHexInt64(&hexNumber) else { return .clear }
+
+        let red, green, blue: CGFloat
+
+        red = CGFloat((hexNumber & 0xff0000) >> 16) / 255
+        green = CGFloat((hexNumber & 0x00ff00) >> 8) / 255
+        blue = CGFloat((hexNumber & 0x0000ff)) / 255
+
+        return UIColor(red: red, green: green, blue: blue, alpha: 1)
+    }
+
+    init(_ photo: Photo) {
+        self.photo = photo
+    }
+
+    subscript<MemberType>(dynamicMember keyPath: KeyPath<Photo, MemberType>) -> MemberType {
+        photo[keyPath: keyPath]
+    }
+}
---
+++ Govenda-iOS Coding Project/ViewControllers/PhotosMainViewController.swift
@@ -0,0 +1,106 @@
+//
+//  PhotosMainViewController.swift
+//  Govenda-iOS Coding Project
+//
+//  Created by James Pamplona on 8/14/23.
+//
+
+import UIKit
+import Combine
+
+
+private let reuseIdentifier = "Cell"
+
+class PhotosMainViewController: UICollectionViewController {
+    let viewModel = PhotosMainViewModel()
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Register cell classes
+        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+
+        viewModel.refreshPhotos(page: 1, photosPerPage: 80)
+        Task.detached { @MainActor in
+
+            for try await _ in self.viewModel.$photos.values {
+                self.collectionView.reloadData()
+            }
+        }
+    }
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        1
+    }
+
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+//        4
+        return viewModel.photos.count
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        let model = viewModel[indexPath.item]
+//        let imageView = UIImageView()
+        let imageView = UILabel()
+        imageView.text = model.altText
+        imageView.backgroundColor = model.averageColor
+
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        cell.contentView.addSubview(imageView)
+        NSLayoutConstraint.activate([
+            imageView.leadingAnchor.constraint(equalTo: cell.contentView.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: cell.contentView.trailingAnchor),
+            imageView.topAnchor.constraint(equalTo: cell.contentView.topAnchor),
+            imageView.bottomAnchor.constraint(equalTo: cell.contentView.bottomAnchor),
+        ])
+        /*
+        Task.detached { @MainActor in
+            imageView.image = try? await model.thumbnail
+        }
+        */
+
+        return cell
+    }
+
+    // MARK: UICollectionViewDelegate
+
+    /*
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
+    
+    }
+    */
+
+}
